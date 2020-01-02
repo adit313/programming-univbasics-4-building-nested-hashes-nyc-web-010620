0426ec181f630dc095471f6235251d592167e4cc
Adit P Patel <app2116@gmail.com>
2020-01-02 01:59:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  {railroads: {pieces: 4, rent_in_dollars: {alpha: , beta: , delta: , gamma: }}}
+  {railroads: {pieces: 4, rent_in_dollars: {alpha: 1, beta: 1, delta: 1, gamma: 1 }}}
 end
 
 def monopoly_with_fourth_tier
